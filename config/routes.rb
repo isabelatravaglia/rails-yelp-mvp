7a4e842917a199e830c9b0ca7d303b32a2d43099
Isabela Santos <isabelatravaglia@gmail.com>
2019-08-21 14:56:42 +0100

    basic Yelp MVP done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'restaurants#index'
+  # get 'reviews/new'
+  # get 'reviews/create'
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  puts "building restaurants"
+Restaurant.create(name: Faker::Restaurant.name,
+  address: Faker::Address.street_name,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+puts "done"
+end
